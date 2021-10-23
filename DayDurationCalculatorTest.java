f293c6ff3ac6808335949d0a0a4eda440b6eb6e2
MahnoorFatima12 <50354886+MahnoorFatima12@users.noreply.github.com>
2021-10-23 00:01:51 -0500

    Added testcase3

--- DayDurationCalculatorTest.java
+++ DayDurationCalculatorTest.java
@@ -12,3 +12,7 @@ public class DaysDurationCalculatorTest {
 	public final void testCal2() {
 		assertNotEquals(dayz.cal(3, 4, 1, 8, 2001),dayz.cal(4, 10, 1, 7 , 2020));
 	}
+@Test
+	public final void testCal3() {
+		assertEquals(dayz.cal(3, 4, 1, 8, 2001),dayz.cal(3, 4, 1, 8, 2001));
+	}
