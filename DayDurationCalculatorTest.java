28c3705ec7826d0fc9e45f7eb4056b91ee2c81dc
MahnoorFatima12 <50354886+MahnoorFatima12@users.noreply.github.com>
2021-10-22 23:59:12 -0500

    Add testcase1

--- DayDurationCalculatorTest.java
+++ DayDurationCalculatorTest.java
@@ -4,3 +4,7 @@ import org.junit.Test;
 
 public class DaysDurationCalculatorTest {
 	DaysDurationCalculator dayz = new DaysDurationCalculator();
+@Test
+	public final void testCal() {
+		assertNotNull(dayz.cal(0, 0, 0, 0, 0));
+	}
